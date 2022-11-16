8a375f33ad23cbacb0d7f93dce8a69a13f63bdde
k-roozbahani <kavehrozbah@gmail.com>
2022-11-16 22:06:20 +0330

    create def movies-list

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
 
-# Create your views here.
+
+def movies_list(request):
+    limit = int(request.GET.get('limit', 8))
+    offset = int(request.GET.get('offset', 0))
+    movies = Movie.objects.all()[offset:limit]
+    return render(request, 'movies_list.html', context={'movies': movies})gi
+
+
+def movies_detail(request, pk):
+    if request.method == 'GET':
+        movie = get_object_or_404(Movie, pk=pk, is_valid=True)
+        content = {'movie': movie}
+        return render(request, 'movie_detail.html', context=content)
