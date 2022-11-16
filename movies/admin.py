6e37be666c6a27b49ff45a93c9ec57b4f4916f80
k-roozbahani <kavehrozbah@gmail.com>
2022-11-16 22:10:37 +0330

    make admin site

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Movie, Role, Crew, Genre, MovieCrew
 
-# Register your models here.
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+class MCInline(admin.TabularInline):
+    model = MovieCrew
+    extra = 1
+
+
+class GenreInline(admin.StackedInline):
+    model = Movie.genres.through
+    extra = 1
+
+
+class CrewAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+    inlines = [MCInline]
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'release_date']
+    search_fields = ['title']
+    inlines = [MCInline, GenreInline]
+    exclude = ('genres',)
+
+
+admin.site.register(Role, RoleAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Crew, CrewAdmin)
+
