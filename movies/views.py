76249bb023e327ca3b787f5614250d21745fb559
k-roozbahani <kavehrozbah@gmail.com>
2022-11-17 17:12:57 +0330

    complate temp

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
+import django.contrib.staticfiles
 
 
 def movies_list(request):
-    limit = int(request.GET.get('limit', 8))
-    offset = int(request.GET.get('offset', 0))
-    movies = Movie.objects.all()[offset:limit]
-    return render(request, 'movies_list.html', context={'movies': movies})gi
+    # limit = int(request.GET.get('limit', 8))
+    # offset = int(request.GET.get('offset', 0))
+    movies = Movie.objects.all()
+    return render(request, 'movies_list.html', context={'movies': movies})
 
 
 def movies_detail(request, pk):
